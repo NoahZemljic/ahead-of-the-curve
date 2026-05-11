cdd7be2812ddf8b2dbb9f54ffa959d2bab692a3c
Noah Zemljic <noahzemljic@icloud.com>
2026-05-11 22:21:38 +0200

    Remove config.py

--- src/training_pipeline/config.py
+++
@@ -1,6 +0,0 @@
-TEST_SIZE = 0.2
-RANDOM_STATE = 42
-
-REGRESSION_MODEL_NAME = "frontier-download-regressor"
-CLASSIFICATION_MODEL_NAME = "frontier-top-quartile-classifier"
-MLFLOW_EXPERIMENT_NAME = "ahead-of-the-curve"
